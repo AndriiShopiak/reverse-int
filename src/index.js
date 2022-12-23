647b40304ef238aabb4a6e71eaad8e95fb94ac10
Andrii Shopiak <andriyshopyak@gmail.com>
2022-12-23 11:05:12 +0200

    feat: add solve and libraries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+let numStr = Math.abs(n).toString();
+let res = "";
+
+for (let i = numStr.length -1; i >= 0; i--) {
+    res += numStr[i];
+}
+return Number(res);
 }
